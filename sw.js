91ccb4c5fbaea24b5c953e511ff6fd4ce07722ce
Seven0115-777 <tianqiplus@163.com>
2026-07-08 20:06:30 +0800

    add weight tracking (#3)
    Co-authored-by: Seven0115-777 <Seven0115-777@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "food-log-v6";
-const STATIC = ["./", "./app.js?v=6", "./styles.css?v=6", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "food-log-v7";
+const STATIC = ["./", "./app.js?v=7", "./styles.css?v=7", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)));
